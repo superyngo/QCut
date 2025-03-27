d3be3432631027a162254a0c8306c254e6fb835c
superyngo <superyngo@gmail.com>
2025-03-27 22:38:39 +0800

    test

--- src/main_converter.py
+++ src/main_converter.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 
 def main() -> None:
-    target_path: Path = Path(r"C:\Users\user\Downloads")
+    target_path: Path = Path(r"F:\Users\user\Downloads")
     mideo_converter.MergeByDate(
         input_folder_path=target_path,
         valid_extensions={mideo_converter.VideoSuffix.MP4},
@@ -15,7 +15,7 @@ def main() -> None:
 
     mideo_converter.BatchVideoRender(
         input_folder_path=target_path,
-        output_folder_path=target_path / "cut_sl",
+        output_folder_path=target_path / "partitioned",
         valid_extensions={mideo_converter.VideoSuffix.MKV},
         walkthrough=False,
         delete_after=False,
@@ -24,7 +24,47 @@ def main() -> None:
         ),
     ).apply(
         task=mideo_converter.ffmpeg_toolkit.PARTIAL_TASKS.partion_video(
-            output_dir=target_path / "partitioned"
+            output_dir=target_path / "partitioned",
+            partition_config=mideo_converter.ffmpeg_toolkit.PartitionConfig(
+                portion_method=[
+                    (1, mideo_converter.ffmpeg_toolkit.PARTIAL_TASKS.speedup()),
+                    (2, mideo_converter.ffmpeg_toolkit.PARTIAL_TASKS.cut_silence()),
+                    (3, mideo_converter.ffmpeg_toolkit.PARTIAL_TASKS.cut_motionless()),
+                    (4, mideo_converter.ffmpeg_toolkit.PARTIAL_TASKS.jumpcut()),
+                    (
+                        5,
+                        mideo_converter.ffmpeg_toolkit.PARTIAL_TASKS.partion_video(
+                            output_dir=target_path / "partitioned4",
+                            partition_config=mideo_converter.ffmpeg_toolkit.PartitionConfig(
+                                portion_method=[
+                                    (
+                                        1,
+                                        mideo_converter.ffmpeg_toolkit.PARTIAL_TASKS.speedup(),
+                                    ),
+                                    (
+                                        2,
+                                        mideo_converter.ffmpeg_toolkit.PARTIAL_TASKS.cut_silence(),
+                                    ),
+                                    (
+                                        3,
+                                        mideo_converter.ffmpeg_toolkit.PARTIAL_TASKS.cut_motionless(),
+                                    ),
+                                    (
+                                        4,
+                                        mideo_converter.ffmpeg_toolkit.PARTIAL_TASKS.jumpcut(),
+                                    ),
+                                    (
+                                        5,
+                                        mideo_converter.ffmpeg_toolkit.PARTIAL_TASKS.partion_video(
+                                            output_dir=target_path / "partitioned44",
+                                        ),
+                                    ),
+                                ]
+                            ),
+                        ),
+                    ),
+                ]
+            ),
         )
     )
 
